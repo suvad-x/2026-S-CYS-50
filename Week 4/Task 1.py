b511967c433df54324bf02a048d8a0b282672bb3
Dawar <155221945+suvad-x@users.noreply.github.com>
2026-06-06 12:27:04 -0700

    Delete Week 4 directory

--- Week 4/Task 1.py
+++
@@ -1,5 +0,0 @@
-def greet():
-    message = "Welcome to Python Programming"
-    print(message)
-for i in range(2):
-    greet()
--- Week 4/Task 2.py
+++
@@ -1,4 +0,0 @@
-def displayName(name):
-    print("Hello", name)
-name = input("Enter your name: ")
-displayName(name)
