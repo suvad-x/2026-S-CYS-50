dd78748d782486ce8c911c934a2befd7f371e132
Dawar <155221945+suvad-x@users.noreply.github.com>
2026-06-07 00:27:30 +0500

    Add files via upload

---
+++ week 4/Task 2.py
@@ -0,0 +1,4 @@
+def displayName(name):
+    print("Hello", name)
+name = input("Enter your name: ")
+displayName(name)
---
+++ week 4/Task 3.py
@@ -0,0 +1,7 @@
+def addNumbers(a, b):
+    return a + b
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = addNumbers(num1, num2)
+print("Sum is:", result)
